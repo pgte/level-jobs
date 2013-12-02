3951c4686420f4a5b70fdf25607ecc732d912c88
Tim <secoif@gmail.com>
2013-12-02 22:07:31 +1000

    Add client for creating jobs but not defining them
    Only pass in the correct sublevel to start creating jobs.

---
+++ client.js
@@ -0,0 +1,57 @@
+var assert       = require('assert');
+var inherits     = require('util').inherits;
+var EventEmitter = require('events').EventEmitter;
+var Sublevel     = require('level-sublevel');
+var stringify    = require('json-stringify-safe');
+var xtend        = require('xtend');
+var timestamp    = require('./timestamp');
+
+exports = module.exports = function Jobs(db, worker, options) {
+  assert.equal(typeof db, 'object', 'need db');
+  assert.equal(arguments.length, 1, 'cannot define worker on client');
+
+  return new Queue(db);
+};
+
+function Queue(db) {
+  EventEmitter.call(this);
+
+  this._db = db = Sublevel(db);
+  this._work = db.sublevel('work');
+};
+
+inherits(Queue, EventEmitter);
+
+var Q = Queue.prototype;
+
+/// push
+
+Q.push = function push(payload, cb) {
+  var q = this;
+  var id = timestamp();
+  this._work.put(id, stringify(payload), put);
+
+  return id;
+
+  function put(err) {
+    if (err) {
+      if (cb) cb(err);
+      else q.emit('error', err);
+    } else if (cb) cb();
+  };
+};
+
+/// del
+
+Q.del = function del(id, cb) {
+  this._work.del(id, cb);
+};
+
+/// readStream
+
+Q.readStream = function readStream(options) {
+  if (! options) options = {};
+  options = xtend({}, options);
+  options.valueEncoding = 'json';
+  return this._work.createReadStream(options);
+};
---
+++ tests/client.js
@@ -0,0 +1,81 @@
+var test       = require('tap').test;
+var rimraf     = require('rimraf');
+var level      = require('level');
+var async      = require('async');
+var Jobs       = require('../');
+var ClientJobs = require('../client');
+
+var dbPath = __dirname + '/db';
+
+test('client', function(t) {
+
+  rimraf.sync(dbPath);
+  var db = level(dbPath);
+
+  var max = 10;
+  var queue = Jobs(db, worker);
+
+  var clientQueue = ClientJobs(db);
+
+  for (var i = 1 ; i <= max ; i ++) {
+    clientQueue.push({n:i}, pushed);
+  }
+
+  function pushed(err) {
+    if (err) throw err;
+  }
+
+  var count = 0;
+  var cbs = [];
+  function worker(work, cb) {
+    count ++;
+    t.equal(work.n, count);
+    cbs.push(cb);
+    if (count == max) callback();
+  };
+
+  function callback() {
+    while(cbs.length) cbs.shift()();
+  }
+
+  queue.on('drain', function() {
+    if (count == max) {
+      t.equal(cbs.length, 0);
+      t.equal(queue._concurrency, 0);
+      db.once('closed', t.end.bind(t));
+      db.close();
+    }
+  });
+
+})
+
+
+test('can delete job', function(t) {
+  rimraf.sync(dbPath);
+  var db = level(dbPath);
+  var jobs = Jobs(db, worker);
+
+  var clientQueue = ClientJobs(db);
+  var processed = 0;
+
+  function worker (payload, done) {
+    processed += 1;
+    t.ok(processed <= 1, 'worker is not called 2 times');
+
+    jobs.del(job2Id, function(err) {
+      if (err) throw err;
+      done();
+    });
+
+    setTimeout(function() {
+      db.once('closed', t.end.bind(t));
+      db.close();
+    }, 500);
+  };
+
+  var job1Id = clientQueue.push({ foo: 'bar', seq: 1 });
+  t.type(job1Id, 'number');
+
+  var job2Id = clientQueue.push({ foo: 'bar', seq: 2 });
+  t.type(job2Id, 'number');
+});
